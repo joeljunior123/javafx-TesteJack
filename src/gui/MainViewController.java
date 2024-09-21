b4e79c700d986985bf68fb917da1d1b4e534eba4
Joel Jr <joelaraujojuca10@gmail.com>
2024-09-21 14:48:57 -0300

    Main View Controller

---
+++ src/gui/MainViewController.java
@@ -0,0 +1,33 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+public class MainViewController implements Initializable {
+
+	@FXML
+	private MenuItem menuItemWithdraw;
+	
+	@FXML
+	private MenuItem menuItemAbout;
+	
+	@FXML
+	public void onMenuItemWithdrawAction() {
+		System.out.println("Teste");
+	}
+	
+	@FXML
+	public void onMenuItemAboutAction() {
+		System.out.println("TesteAbout");
+	}
+	
+	
+	@Override
+	public void initialize(URL uri, ResourceBundle rb) {		
+	}
+
+}
